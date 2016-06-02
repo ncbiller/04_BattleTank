f3498806f0d9c0ae5bd1c35c4b531946ee3786a5
Nicholas Biller <nick@biller.me.uk>
2016-06-02 18:37:16 +0100

    Final Branch 1 Commit

---
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "Tank.h"
+
+
+// Sets default values
+ATank::ATank()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	UE_LOG(LogTemp, Warning, TEXT("TankConstructor Called"));
+}
+
+// Called when the game starts or when spawned
+void ATank::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATank::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+// Called to bind functionality to input
+void ATank::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+	
+	InputComponent->BindAction("Turret_Clockwise", IE_Pressed, this, &ATank::RotateTurretClockwise);
+	//Turret_CounterClockwise
+	InputComponent->BindAction("Turret_CounterClockwise", IE_Pressed, this, &ATank::RotateTurretCounterClockwise);
+}
+
+void ATank::SetTurretChildActor(UChildActorComponent * TurretFromBP)
+{
+	Turret = TurretFromBP;
+	UE_LOG(LogTemp, Warning, TEXT("SetTurretChildActor Called"));
+}
+
+void ATank::RotateTurretClockwise() {
+	UE_LOG(LogTemp, Warning, TEXT("RotateTurretClockwise() Called"));
+	Turret->SetRelativeRotation(FRotator(0, 30.f, 0));
+
+}
+
+void ATank::RotateTurretCounterClockwise()
+{
+	UE_LOG(LogTemp, Warning, TEXT("RotateTurretCounterClockwise() Called"));
+	Turret->SetRelativeRotation(FRotator(0, -30.f, 0));
+
+}
