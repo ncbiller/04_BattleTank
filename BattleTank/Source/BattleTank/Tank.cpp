89b3859c2c09be5b0fefe20ad1c5103a17d0a0ea
Nicholas Biller <nick@biller.me.uk>
2016-07-05 19:40:05 +0100

    124: Creating Default SubObjects in C++  - Tank Aiming Component Created in Code and Used to delegate Aiming

--- BattleTank/Source/BattleTank/Tank.cpp
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -9,7 +9,10 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	UE_LOG(LogTemp, Warning, TEXT("TankConstructor Called"));
+	
+	//No need to protect pointers as added at construction
+
+	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
 }
 
 // Called when the game starts or when spawned
@@ -57,7 +60,5 @@ void ATank::RotateTurretCounterClockwise()
 
 void ATank::AimAt(FVector HitLocation) {
 
-	auto OurTankName = GetName();
-
-	UE_LOG(LogTemp, Warning, TEXT("%s Aiming at: %s"),*OurTankName, *HitLocation.ToString());
+	TankAimingComponent->AimAt(HitLocation);
 }
